b55012532b3121785a3758998ed8dae42ffe2c22
Santiago <slopez@fi.uba.ar>
2021-11-13 00:16:46 -0300

    Create Tile model

---
+++ src/sdl/tile.cpp
@@ -0,0 +1,49 @@
+#include "tile.h"
+
+Tile::Tile(SDL2pp::Renderer &renderer, bool black) : renderer(renderer),
+                                                     sprite(renderer) {
+  if (black) {
+    images = {
+        {DEFAULT, "img/black_square.png"},
+        {MOVE, "img/black_square_move.png"},
+        {ENTANGLE, "img/black_square_entangle.png"},
+        {SPLIT, "img/black_square_split.png"}
+    };
+  } else {
+    images = {
+        {DEFAULT, "img/white_square.png"},
+        {MOVE, "img/white_square_move.png"},
+        {ENTANGLE, "img/white_square_relation.png"},
+        {SPLIT, "img/white_square_split.png"}
+    };
+  }
+  sprite.loadImage(images[DEFAULT]);
+}
+
+Tile::Tile(Tile &&other) noexcept: renderer(other.renderer),
+                                   sprite(std::move(other.sprite)),
+                                   images(std::move(other.images)) {}
+
+void Tile::render(int x_pos, int y_pos) {
+  int width, height;
+  width = height = renderer.GetOutputHeight() / 10;
+  int middle_board = std::min(renderer.GetOutputHeight(),
+                                                renderer.GetOutputWidth()) / 2;
+  x_pos -= 4;
+  y_pos -= 3;
+  x_pos *= width;
+  y_pos *= -height;
+  sprite.render(middle_board + x_pos, middle_board + y_pos, width, height);
+}
+
+void Tile::renderMove() {
+  sprite.loadImage(images[MOVE]);
+}
+
+void Tile::renderEntangle() {
+  sprite.loadImage(images[ENTANGLE]);
+}
+
+void Tile::renderSplit() {
+  sprite.loadImage(images[SPLIT]);
+}
---
+++ src/sdl/tile.h
@@ -0,0 +1,30 @@
+#ifndef QUANTUM_CHESS_PROJ_TILE_H
+#define QUANTUM_CHESS_PROJ_TILE_H
+
+#include "sprite.h"
+#include <SDL2pp/SDL2pp.hh>
+#include <map>
+#include <string>
+
+class Tile {
+private:
+  SDL2pp::Renderer &renderer;
+  Sprite sprite;
+  std::map<int, std::string> images;
+
+  typedef enum {
+    DEFAULT, MOVE, ENTANGLE, SPLIT
+  } SquareType;
+
+public:
+  Tile(SDL2pp::Renderer &renderer, bool black);
+  Tile(Tile &&other) noexcept;
+//  Tile& operator=(Tile &other);
+  void render(int x, int y);
+  void renderMove();
+  void renderEntangle();
+  void renderSplit();
+};
+
+
+#endif //QUANTUM_CHESS_PROJ_TILE_H

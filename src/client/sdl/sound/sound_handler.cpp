1644d554aba1b500f789dd5a62dabc504489038f
Santiago <slopez@fi.uba.ar>
2021-11-25 17:18:31 -0300

    Fix track directory path

--- src/client/sdl/sound/sound_handler.cpp
+++ src/client/sdl/sound/sound_handler.cpp
@@ -2,33 +2,33 @@
 
 #define NUMBER_OF_DIFFERENT_CHUNKS 5
 
-SoundHandler::SoundHandler(SDL2pp::Mixer& mixer)
-            :mixer(mixer), music("21_sound_effects_and_music/music.wav") {
-    chunks.reserve(NUMBER_OF_DIFFERENT_CHUNKS);
-    chunks.push_back(std::move(SDL2pp::Chunk("21_sound_effects_and_music/low.wav")));
-    chunks.push_back(std::move(SDL2pp::Chunk("21_sound_effects_and_music/high.wav")));
-    chunks.push_back(std::move(SDL2pp::Chunk("21_sound_effects_and_music/medium.wav")));
-    chunks.push_back(std::move(SDL2pp::Chunk("21_sound_effects_and_music/beat.wav")));
+SoundHandler::SoundHandler(SDL2pp::Mixer &mixer)
+    : mixer(mixer), music("sound/music.wav") {
+  chunks.reserve(NUMBER_OF_DIFFERENT_CHUNKS);
+  chunks.push_back(std::move(SDL2pp::Chunk("sound/low.wav")));
+  chunks.push_back(std::move(SDL2pp::Chunk("sound/high.wav")));
+  chunks.push_back(std::move(SDL2pp::Chunk("sound/medium.wav")));
+  chunks.push_back(std::move(SDL2pp::Chunk("sound/beat.wav")));
 }
 
 void SoundHandler::playMusic() const {
-    mixer.PlayMusic(music);
+  mixer.PlayMusic(music);
 }
 
 void SoundHandler::stopMusic() const {
-    mixer.PauseMusic();
+  mixer.PauseMusic();
 }
 
 void SoundHandler::playSplitSound() const {
-    mixer.PlayChannel(-1, chunks[0]);
+  mixer.PlayChannel(-1, chunks[0]);
 }
 
 void SoundHandler::playMovementSound() const {
-    mixer.PlayChannel(-1, chunks[1]);
+  mixer.PlayChannel(-1, chunks[1]);
 }
 
 void SoundHandler::playTakenPieceSound() const {
-    mixer.PlayChannel(-1, chunks[2]);
+  mixer.PlayChannel(-1, chunks[2]);
 }
 
 void SoundHandler::playNewClientSound() const {

a7bb75b5457e74ad06c230512a5dc2b275818a95
andrezszambrano <andreskilla2000@gmail.com>
2021-11-26 01:15:19 -0300

    DrawableContainer is now a template that can contain any drawable class

--- src/client/sdl/chat/drawable_container.h
+++ src/client/sdl/chat/drawable_container.h
@@ -5,20 +5,49 @@
 #include "drawable_text.h"
 #include <list>
 
+#define NO_LIMIT -1
+
+
+Template <class T>
 class DrawableContainer {
 private:
-    std::list<DrawableText> drawables_list;
+    std::list<T> drawables_list;
     int max_drawables;
 
 public:
     DrawableContainer();
 
-    DrawableContainer(int max_drawables_);
+    DrawableContainer(unsigned int max_drawables_);
 
-    void addDrawableToList(DrawableText&& drawable);
+    void addDrawableToList(T&& drawable);
 
     void drawAllDrawablesStartingFrom(int x, int y);
 };
 
+template <class T>
+DrawableContainer<T>::DrawableContainer()
+                    :max_drawables(NO_LIMIT) {
+}
+
+template <class T>
+DrawableContainer<T>::DrawableContainer(unsigned int max_drawables_);
+        :max_drawables(max_drawables_) {
+}
+
+template <class T>
+void DrawableContainer<T>::addDrawableToList(T&& drawable) {
+    if (this->drawables_list.size() == this->max_drawables)
+        this->drawables_list.pop_front();
+    this->drawables_list.push_back(std::move(drawable));
+}
+
+template <class T>
+void DrawableContainer<T>::drawAllDrawablesStartingFrom(int x, int y) {
+    int i = 0;
+    for (auto const& drawable : this->drawables_list) {
+        drawable.drawIn(x, y - i);
+        i++;  //i is increased respectively
+    }
+}
 
 #endif //QUANTUM_CHESS_PROJ_DRAWABLE_CONTAINER_H

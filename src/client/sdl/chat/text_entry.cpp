adb83bb2fa6ea3f544154dd7f9966422ab852118
Santiago <slopez@fi.uba.ar>
2021-11-26 18:30:20 -0300

    Handle text through text_entry

--- src/client/sdl/chat/text_entry.cpp
+++ src/client/sdl/chat/text_entry.cpp
@@ -1,31 +1,21 @@
 #include "text_entry.h"
 #include "../renderer.h"
 
-TextEntry::TextEntry(Renderer &renderer_, Font &font_) : renderer(renderer_),
-                                                         sprite(renderer_,
-                                                                font_,
-                                                                " "),
-                                                         font(font_),
-                                                         string("") {
+TextEntry::TextEntry(Renderer &renderer_, Font &font_) : text(renderer_,
+                                                              font_) {
   SDL_StartTextInput();
 }
 
-void TextEntry::add(char c) {
-  string.push_back(c);
+void TextEntry::add(const std::string &str) {
+  text.concat(str);
 }
 
-void TextEntry::add(const std::string &text) {
-  string += text;
-}
-
-void TextEntry::erase() {
-  if (not string.empty())
-    string.erase(string.end());
+void TextEntry::backspace() {
+  text.delete_last();
 }
 
 void TextEntry::render(int x, int y) {
-  if (not string.empty())
-    sprite.render(string, x, y);
+  text.render(x, y, 100, 10);
 }
 
 TextEntry::~TextEntry() {
--- src/client/sdl/chat/text_entry.h
+++ src/client/sdl/chat/text_entry.h
@@ -1,29 +1,23 @@
 #ifndef QUANTUM_CHESS_PROJ_TEXT_ENTRY_H
 #define QUANTUM_CHESS_PROJ_TEXT_ENTRY_H
 
-#include "../window.h"
+#include "../renderer.h"
 #include "font.h"
 #include "text_sprite.h"
+#include "text.h"
 #include <string>
 
 class TextEntry {
-//  Font &font;
-  Renderer &renderer;
-  TextSprite sprite;
-  Font &font;
-  std::string string;
+  Text text;
 
 public:
   TextEntry(Renderer &renderer_, Font &font_);
-
-  // Add character at the end
-  void add(char c);
 
   // Concatenate string
   void add(const std::string &text);
 
   // Delete last character
-  void erase();
+  void backspace();
 
   void render(int x, int y);
 

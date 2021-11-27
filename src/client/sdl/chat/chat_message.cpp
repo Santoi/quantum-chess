9326635bb35eb1c4af6a6401fec771ad7bd1932d
Santiago <slopez@fi.uba.ar>
2021-11-27 16:23:00 -0300

    Chat message structurer and renderer

--- src/client/sdl/chat/chat_message.cpp
+++ src/client/sdl/chat/chat_message.cpp
@@ -1,1 +1,26 @@
 #include "chat_message.h"
+
+ChatMessage::ChatMessage(Renderer &renderer, Font &font,
+                         const std::string &name, const std::string &id,
+                         const std::string &time, const std::string &message) :
+    renderer(renderer),
+    font(font),
+    name(renderer, font, name, 'w'),
+    id(renderer, font, "#" + id, 't'),
+    time(renderer, font, time, 'd'),
+    message(renderer, font, message, 'w') {}
+
+void ChatMessage::render(int x, int y) {
+  int x_pos = x, y_pos = y;
+  name.render(x_pos, y_pos);
+  x_pos += name.getTextOutputWidth();
+  id.render(x_pos, y_pos);
+  x_pos += id.getTextOutputWidth();
+  DrawableText colon(renderer, font, ": ");
+  colon.render(x_pos, y_pos);
+  x_pos += colon.getTextOutputWidth();
+  time.render(x_pos, y_pos);
+  y_pos += name.getTextOutputHeight();
+  x_pos = x;
+  message.render(x_pos, y_pos);
+}
--- src/client/sdl/chat/chat_message.h
+++ src/client/sdl/chat/chat_message.h
@@ -1,9 +1,25 @@
 #ifndef QUANTUM_CHESS_PROJ_CHAT_MESSAGE_H
 #define QUANTUM_CHESS_PROJ_CHAT_MESSAGE_H
 
+#include "../renderer.h"
+#include "font.h"
+#include "drawable_text.h"
+#include <string>
 
-class chat_message {
+class ChatMessage {
+  Renderer &renderer;
+  Font &font;
+  DrawableText name;
+  DrawableText id;
+  DrawableText time;
+  DrawableText message;
 
+public:
+  ChatMessage(Renderer &renderer, Font &font,
+              const std::string &name, const std::string &id,
+              const std::string &time, const std::string &message);
+
+  void render(int x, int y);
 };
 
 

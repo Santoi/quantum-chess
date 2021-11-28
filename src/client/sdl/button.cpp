a72d37734cf10033c31d35d626079a9a503186c4
andrezszambrano <andreskilla2000@gmail.com>
2021-11-28 14:34:41 -0300

    Class Button is now an abstract class that has knowledge of its position and area. ConnectButton class, that derives from Button, is created.

--- src/client/sdl/button.cpp
+++ src/client/sdl/button.cpp
@@ -1,6 +1,20 @@
 #include "button.h"
 
-bool Button::fillTokensIfClicked(const PixelCoordinate& pixel_, std::list<std::string>& tokens) {
+void Button::setAreaAndPosition(int x_, int y_, int height_, int width_) {
+    x = x_;
+    y = y_;
+    height = height_;
+    width = width_;
+}
+
+ConnectButton::ConnectButton(const std::list<std::unique_ptr<TextEntryButton>>& text_entry_buttons_ptr)
+                :text_entries(text_entry_buttons_ptr) {
+}
+
+bool ConnectButton::fillTokensIfClicked(const PixelCoordinate& pixel_, std::list<std::string>& tokens) {
     return false;
 }
 
+bool PickMatchButton::fillTokensIfClicked(const PixelCoordinate& pixel_, std::list<std::string>& tokens) {
+    return false;
+}
--- src/client/sdl/button.h
+++ src/client/sdl/button.h
@@ -2,15 +2,47 @@
 #define QUANTUM_CHESS_PROJ_BUTTON_H
 
 #include "pixel_coordinate.h"
+#include "text_entry_button.h"
 #include <list>
 #include <string>
+#include <memory>
+
 class Button {
+private:
+    int x;
+    int y;
+    int height;
+    int width;
 
 public:
+
     Button() = default;
+
+    virtual bool fillTokensIfClicked(const PixelCoordinate& pixel_, std::list<std::string>& tokens) = 0;
+
+    void setAreaAndPosition(int x, int y, int height, int width);
+};
+
+class ConnectButton: public Button {
+private:
+    const std::list<std::unique_ptr<TextEntryButton>>& text_entries;
+
+public:
+    ConnectButton() = delete;
+
+    explicit ConnectButton(const std::list<std::unique_ptr<TextEntryButton>>& text_entry_buttons_ptr);
 
     bool fillTokensIfClicked(const PixelCoordinate& pixel_, std::list<std::string>& tokens);
 
 };
+
+class PickMatchButton: public Button {
+
+public:
+
+    bool fillTokensIfClicked(const PixelCoordinate& pixel_, std::list<std::string>& tokens);
+
+};
+
 
 #endif //QUANTUM_CHESS_PROJ_BUTTON_H

b0b94353ec731d08b39eee3cbaa73e8e044b4ee7
Santiago <slopez@fi.uba.ar>
2021-11-28 16:08:50 -0300

    Create sdl and mixer inside the window

--- src/client/sdl/window.cpp
+++ src/client/sdl/window.cpp
@@ -4,16 +4,24 @@
 #define DEFAULT_HEIGHT 480
 #define DEFAULT_WIDTH 640
 
-Window::Window(): window_("Quantum Chess",
-                        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                        DEFAULT_WIDTH, DEFAULT_HEIGHT,
-                        SDL_WINDOW_RESIZABLE),
-                  renderer_(window_) {}
+Window::Window() :
+    sdl(SDL_INIT_VIDEO | SDL_INIT_AUDIO),
+    mixer(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 4096),
+    window_("Quantum Chess",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            DEFAULT_WIDTH, DEFAULT_HEIGHT,
+            SDL_WINDOW_RESIZABLE),
+    renderer_(window_),
+    sound_handler_(mixer) {}
 
-SDL2pp::Window& Window::window() {
+SDL2pp::Window &Window::window() {
   return window_;
 }
 
-Renderer& Window::renderer() {
+Renderer &Window::renderer() {
   return renderer_;
+}
+
+SoundHandler &Window::sound_handler() {
+  return sound_handler_;
 }

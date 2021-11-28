172c358225d0d5f951fed75a9f37593f37749b99
andrezszambrano <andreskilla2000@gmail.com>
2021-11-28 15:36:11 -0300

    ConnectButton now has the logic of filling the tokens list with the text entries' text. It does it in the order that the NotConnectedToServerState has its list of text entries.

--- src/client/sdl/button.cpp
+++ src/client/sdl/button.cpp
@@ -9,9 +9,15 @@ void Button::setAreaAndPosition(int x_, int y_, int height_, int width_) {
 
 ConnectButton::ConnectButton(const std::list<std::unique_ptr<TextEntryButton>>& text_entry_buttons_ptr)
                 :text_entries(text_entry_buttons_ptr) {
+
 }
 
 bool ConnectButton::fillTokensIfClicked(const PixelCoordinate& pixel_, std::list<std::string>& tokens) {
+    //if (wasClicked)
+      for (auto it = text_entries.begin(); it != text_entries.end(); it++)
+            tokens.push_back((**it).getText());
+    //  return true
+    //else
     return false;
 }
 

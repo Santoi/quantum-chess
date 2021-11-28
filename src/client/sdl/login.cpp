5d277a936a09098d179539be8c94ab9557183fa3
andrezszambrano <andreskilla2000@gmail.com>
2021-11-28 00:01:16 -0300

    Login class now does not derive from Scene class. All logic about rendering/buttons is deleted from class

--- src/client/sdl/login.cpp
+++ src/client/sdl/login.cpp
@@ -1,44 +1,15 @@
 #include "login.h"
 
-#define NUMBER_OF_BUTTONS 5
-#define ENTER_IP_BUTTON 0
-#define ENTER_PORT_BUTTON 1
-
-Login::Login(Window& window)
-        :Scene(window.renderer().getMinDimension()), client_socket_ptr(nullptr) {
+Login::Login()
+        :client_socket_ptr(nullptr) {
 }
 
-bool Login::clientIsConnectedToMatch() {
-    return client_is_connected_to_match;
-}
-
-
-void Login::setScale(int scale_) {
-    scale = scale_;
-}
-
-void Login::render() {
-    if (!client_is_connected_to_server)
-        renderGetIPandPort();
-}
-
-void Login::renderGetIPandPort() {
-
-    std::cout << "Ingresa el IP" << std::endl;
-}
-
-
-bool Login::pixelIsOnAButton(const PixelCoordinate& pixel_) {
-    if (!client_is_connected_to_server)
-        return connect_to_server_button.pixelIsOnButton(pixel_);
-    return false;
-}
-
+/*
 void Login::pressButtonThatHasPixel(const PixelCoordinate& pixel_) {
     std::string result;
     if (!client_is_connected_to_server) {
        try {
-           //get info from text fields (ip, port, nick name)
+           //get info from text fields network container (ip, port, nick name)
            char* ip = nullptr;
            char* port = nullptr;
            Socket::createAConnectedSocketPointedByUniquePointer(client_socket_ptr, ip, port);
@@ -46,7 +17,7 @@ void Login::pressButtonThatHasPixel(const PixelCoordinate& pixel_) {
 
        }
     }
-}
+}*/
 
 Socket Login::getClientSocket() {
     return std::move(*client_socket_ptr);
--- src/client/sdl/login.h
+++ src/client/sdl/login.h
@@ -6,42 +6,25 @@
 #include "../../common/src/blocking_queue.h"
 #include "../../common/src/socket.h"
 #include "button.h"
-#include "../sdl/window.h"
 #include <memory>
 #include <vector>
 #include <mutex>
 #include <string>
 
-class Login: public Scene {
+class Login {
 private:
     std::mutex mutex;
     std::unique_ptr<Socket> client_socket_ptr;
-    bool client_is_connected_to_match;
-    bool client_is_connected_to_server;
-    Button connect_to_server_button;
     std::string client_nick_name;
 
 public:
-    Login() = delete;
 
-    Login(Window& window);
-
-    bool clientIsConnectedToMatch();
-
-    void setScale(int scale_) override;
-
-    void render() override;
-
-    bool pixelIsOnAButton(const PixelCoordinate& pixel_);
-
-    void pressButtonThatHasPixel(const PixelCoordinate& pixel_);
+    Login();
 
     Socket getClientSocket();
 
     std::string getClientNickName();
 
-private:
-    void renderGetIPandPort();
 };
 
 #endif //QUANTUM_CHESS_PROJ_LOGIN_H

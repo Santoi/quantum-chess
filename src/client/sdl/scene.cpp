16ff6ce9d39b8e967d950832d4dd91378ec47473
Santiago <slopez@fi.uba.ar>
2021-11-28 23:16:56 -0300

    Render drawable containers

--- src/client/sdl/scene.cpp
+++ src/client/sdl/scene.cpp
@@ -1,1 +1,42 @@
 #include "scene.h"
+#include "window.h"
+
+#define MAX_CHAT_MESSAGES 3
+#define MAX_LOG_MESSAGES 3
+#define MAX_ERROR_LOG_MESSAGES 1
+
+#define CHAT_WIDTH 50
+
+Scene::Scene(Window &window, Board &board) : window(window), chess(board),
+                                             chat(MAX_CHAT_MESSAGES),
+                                             log(MAX_LOG_MESSAGES), error_log(
+        MAX_ERROR_LOG_MESSAGES) {}
+
+void Scene::render() {
+  int width = window.getWidth(), height = window.getHeight();
+  float total_messages =
+      MAX_CHAT_MESSAGES + MAX_LOG_MESSAGES + MAX_ERROR_LOG_MESSAGES;
+  chess.render(transformer, window.getWidth() - CHAT_WIDTH, window.getHeight());
+  error_log.render(width - CHAT_WIDTH, 0);
+  log.render(width - CHAT_WIDTH,
+             height * (MAX_ERROR_LOG_MESSAGES / total_messages));
+  chat.render(width - CHAT_WIDTH,
+              height *
+              ((MAX_ERROR_LOG_MESSAGES + MAX_LOG_MESSAGES) / total_messages));
+}
+
+int Scene::getChatWidth() const {
+  return CHAT_WIDTH;
+}
+
+int Scene::getChatHeight() const {
+  return window.getHeight();
+}
+
+int Scene::getChessWidth() const {
+  return window.getWidth() - CHAT_WIDTH;
+}
+
+int Scene::getChessHeight() const {
+  return window.getHeight();
+}
--- src/client/sdl/scene.h
+++ src/client/sdl/scene.h
@@ -1,9 +1,33 @@
 #ifndef QUANTUM_CHESS_PROJ_SCENE_H
 #define QUANTUM_CHESS_PROJ_SCENE_H
 
+#include "../game/drawable_chess.h"
+#include "chat/drawable_container.h"
+#include "chat/chat_message.h"
+
+class Window;
 
 class Scene {
+private:
+  Window &window;
+  DrawableChess chess;
+  DrawableContainer<ChatMessage> chat;
+  DrawableContainer<DrawableText> log;
+  DrawableContainer<DrawableText> error_log;
+  CoordinateTransformer transformer;
 
+public:
+  Scene(Window &window, Board &board);
+
+  void render();
+
+  int getChatWidth() const;
+
+  int getChatHeight() const;
+
+  int getChessWidth() const;
+
+  int getChessHeight() const;
 };
 
 

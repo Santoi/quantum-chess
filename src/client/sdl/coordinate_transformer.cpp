bc8ef360c16e7d63f23c72026491d785c4ae951f
Santiago <slopez@fi.uba.ar>
2021-11-28 23:37:00 -0300

    Broken transformation

--- src/client/sdl/coordinate_transformer.cpp
+++ src/client/sdl/coordinate_transformer.cpp
@@ -1,29 +1,33 @@
 #include "coordinate_transformer.h"
 
 void CoordinateTransformer::pixel2Position(const PixelCoordinate &pixel,
-                                           Position &position,
-                                           int scale_factor) {
+                                           Position &position, int width,
+                                           int height) {
   int x = pixel.x(), y = pixel.y();
   x *= 10;
   y *= 10;
-  x /= scale_factor;
-  y /= -scale_factor;
-  x -= 1;
+  x /= height;
+  y /= -height;
+  x -= (width * 10) / (2 * height) - 4;
   y += 8;
   Position pos(x, y);
   position = pos;
 }
 
 void CoordinateTransformer::position2Pixel(const Position &position,
-                                           PixelCoordinate &pixel,
-                                           int scale_factor) {
+                                           PixelCoordinate &pixel, int width,
+                                           int height) {
+
+  std::cerr << "pos2pixel" << std::endl;
   int x = position.x(), y = position.y();
-  x += 1;
+  std::cerr << "pos: " << x << " " << y << std::endl;
+  x += (width * 10) / (2 * height) - 4;
   y -= 8;
-  x *= scale_factor;
-  y *= -scale_factor;
+  x *= height;
+  y *= -height;
   x /= 10;
   y /= 10;
+  std::cerr << "pixel: " << x << " " << y << std::endl;
   PixelCoordinate pc(x, y);
   pixel = pc;
 }

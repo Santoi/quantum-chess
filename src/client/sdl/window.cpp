1fde05e08fbc51c5e5d44895d7a1e1622d63a9a3
Santiago <slopez@fi.uba.ar>
2021-11-30 01:17:01 -0300

    Change inicial resolution

--- src/client/sdl/window.cpp
+++ src/client/sdl/window.cpp
@@ -2,19 +2,19 @@
 #include <SDL2pp/SDL2pp.hh>
 
 #define DEFAULT_HEIGHT 480
-#define DEFAULT_WIDTH 640
+#define DEFAULT_WIDTH 720
 
 #define WINDOW_MIN_RATIO 1.5
 
 Window::Window() :
-        sdl(SDL_INIT_VIDEO | SDL_INIT_AUDIO),
-        mixer(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 4096),
-        window_("Quantum Chess",
-                SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                DEFAULT_WIDTH, DEFAULT_HEIGHT,
-                SDL_WINDOW_RESIZABLE),
-        renderer_(window_),
-        sound_handler_(mixer) {
+    sdl(SDL_INIT_VIDEO | SDL_INIT_AUDIO),
+    mixer(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 4096),
+    window_("Quantum Chess",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            DEFAULT_WIDTH, DEFAULT_HEIGHT,
+            SDL_WINDOW_RESIZABLE),
+    renderer_(window_),
+    sound_handler_(mixer) {
   window_.SetMinimumSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
   window_.SetMaximumSize(SDL_MAX_SINT32, DEFAULT_WIDTH / WINDOW_MIN_RATIO);
 }

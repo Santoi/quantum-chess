6745efc4acbbd801e8dcf5ec75a13adc6357f7f5
Matias Charrut <mcharrut@fi.uba.ar>
2021-11-30 16:51:31 -0300

    Fixed time when hour or minute lower than 10

--- src/server/src/instructions/chat_instruction.cpp
+++ src/server/src/instructions/chat_instruction.cpp
@@ -3,15 +3,15 @@
 
 ChatInstruction::ChatInstruction(const ClientData &instructor_data_,
                                  std::string &&message) :
-    instructor_data(instructor_data_),
-    message(std::move(message)) {}
+        instructor_data(instructor_data_),
+        message(std::move(message)) {}
 
 
 void ChatInstruction::makeActionAndNotifyAllListeningQueues(
-    std::map<uint16_t, BlockingQueue<Instruction>> &listening_queues,
-    Match &match, BlockingQueue<Instruction> &match_updates_queue) {
+        std::map<uint16_t, BlockingQueue<Instruction>> &listening_queues,
+        Match &match, BlockingQueue<Instruction> &match_updates_queue) {
   std::shared_ptr<Instruction> this_instruc_ptr = std::make_shared<ChatInstruction>(
-      instructor_data, std::move(this->message));
+          instructor_data, std::move(this->message));
 
   for (auto &listening_queue: listening_queues)
     listening_queue.second.push(this_instruc_ptr);
@@ -28,7 +28,13 @@ void ChatInstruction::fillPacketWithInstructionsToSend(ServerProtocol &protocol,
 std::string ChatInstruction::getTimeStamp() {
   time_t now = time(nullptr);
   struct tm *ts = localtime(&now);
-  std::string timestamp = std::to_string(ts->tm_hour) + ":" +
-                          std::to_string(ts->tm_min);
+  std::string hour = std::to_string(ts->tm_hour);
+  std::string min = std::to_string(ts->tm_min);
+  // Formatting for 00
+  if (hour.size() == 1)
+    hour = "0" + hour;
+  if (min.size() == 1)
+    min = "0" + min;
+  std::string timestamp = hour + ":" + min;
   return timestamp;
 }

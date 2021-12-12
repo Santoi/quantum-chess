7fd0096bf13d5119a78882938d25754852cac6e7
andrezszambrano <andreskilla2000@gmail.com>
2021-12-12 01:21:43 -0300

    Now to get out of the leave screen window client must press the 'c' key.

--- src/client/sdl/event_handler_thread.cpp
+++ src/client/sdl/event_handler_thread.cpp
@@ -86,17 +86,22 @@ void EventHandlerThread::handleKeyDown() {
     case SDLK_r: {
       if (!text_entry.isEnabled()) {
           std::cout << "r!" << std::endl;
-          if (help_screen_is_being_rendered)
+          if (help_screen_is_being_rendered || leave_screen_is_being_rendered)
               return;
-          if (leave_screen_is_being_rendered) {
-            game_scene.stopRenderingLeaveScreen();
-            leave_screen_is_being_rendered = false;
-          } else {
-            game_scene.startRenderingLeaveScreen();
-            leave_screen_is_being_rendered = true;
-          }
+          game_scene.startRenderingLeaveScreen();
+          leave_screen_is_being_rendered = true;
       }
-        break;
+      break;
+    }
+    case SDLK_c: {
+      if (!text_entry.isEnabled()) {
+        std::cout << "c!" << std::endl;
+        if (!leave_screen_is_being_rendered)
+           return;
+        game_scene.stopRenderingLeaveScreen();
+        leave_screen_is_being_rendered = false;
+      }
+      break;
     }
     case SDLK_n: {
       if (!text_entry.isEnabled())

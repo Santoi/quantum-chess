62fca6202abff6f81d187a1d7eca7d4bb0128899
Santiago <slopez@fi.uba.ar>
2021-12-14 14:15:50 -0300

    Cpplint new files

--- src/server/communication/instructions/surrender_instruction.cpp
+++ src/server/communication/instructions/surrender_instruction.cpp
@@ -6,7 +6,7 @@
 #include <list>
 
 SurrenderInstruction::SurrenderInstruction(const ClientData &instructor_data_)
-                    :instructor_data(instructor_data_) {
+    : instructor_data(instructor_data_) {
 }
 
 void SurrenderInstruction::makeActionAndNotify(Match &match) {
@@ -15,22 +15,23 @@ void SurrenderInstruction::makeActionAndNotify(Match &match) {
       throw ChessException("Spectator cannot surrender");
     match.getBoard().surrender(instructor_data.role ==
                                ClientData::ROLE_WHITE);
-  } catch (const ChessException &e) {
+  } catch(const ChessException &e) {
     std::shared_ptr<Instruction> error_instr =
-            std::make_shared<ChessExceptionInstruction>(instructor_data,
-                                                        e.what());
+        std::make_shared<ChessExceptionInstruction>(instructor_data,
+                                                    e.what());
     match.addInstrToClientListeningQueue(instructor_data.id, error_instr);
     return;
   }
   std::list<std::string> log;
   match.getBoard().popLog(log);
   std::shared_ptr<Instruction> log_ptr = std::make_shared<LogInstruction>(
-          std::move(log));
+      std::move(log));
   match.addInstrToAllListeningQueues(log_ptr);
 }
 
 
-void SurrenderInstruction::fillPacketWithInstructionToSend(ServerProtocol &protocol,
-                                                Packet &packet,
-                                                const ClientData
-                                                &client_receiver_data) {}
+void
+SurrenderInstruction::fillPacketWithInstructionToSend(ServerProtocol &protocol,
+                                                      Packet &packet,
+                                                      const ClientData
+                                                      &client_receiver_data) {}
